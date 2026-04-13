e85b9901c58e954fc815779d938eb92bf2ac31e2
bvancauter <boris.vancauter@gmail.com>
2026-04-13 15:24:08 +0200

    chore: initialize java project

---
+++ projet-bd/src/main/java/be/unamur/infob212/projetbd/ProjetBdApplication.java
@@ -0,0 +1,13 @@
+package be.unamur.infob212.projetbd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetBdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetBdApplication.class, args);
+    }
+
+}
---
+++ projet-bd/src/test/java/be/unamur/infob212/projetbd/ProjetBdApplicationTests.java
@@ -0,0 +1,13 @@
+package be.unamur.infob212.projetbd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetBdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
